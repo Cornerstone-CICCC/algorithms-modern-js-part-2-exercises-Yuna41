f2b96bdcb34506ca4be678b5ea4c4fe795d77382
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-29 21:06:01 -0700

    ex08 completed

--- ex08.js
+++ ex08.js
@@ -1,3 +1,5 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+const words = ["happy", "sad", "angry", "upset", "calm", "lucky"]
+console.log(words.join(" "))
