0079ec615d67bc064766599ed788dd6041eadeeb
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-29 20:57:02 -0700

    ex04 completed

--- ex04.js
+++ ex04.js
@@ -1,3 +1,5 @@
 // Includes Exercise
 // Instructions: Create an array of at least five different fruits as strings. Write code to check if the array includes the fruit "apple".
 
+const fruits = ["apple", "orange", "melon", "strawberry", "grape", "kiwi"]
+console.log(fruits.includes("apple"))
