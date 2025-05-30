b64d4d53da8359124a162acbccdee22ee437a78f
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-29 20:52:36 -0700

    ex02 completed

--- ex02.js
+++ ex02.js
@@ -1,3 +1,10 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
 
+const numbers = [4, 9, 50, 82, 52, 38, 11]
+const numAsc = numbers.sort((a, b) => a > b ? 1 : -1)
+// console.log(numAsc)
+const firstTen = numAsc.find((num) => {
+  return num > 10
+})
+console.log(firstTen)
