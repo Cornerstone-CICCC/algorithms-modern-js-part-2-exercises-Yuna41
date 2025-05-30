39ca03538e1576af4f3e7ebfca34103c0ebb0799
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-29 20:42:57 -0700

    ex01 completed

--- ex01.js
+++ ex01.js
@@ -1,2 +1,10 @@
 // Sort Exercise
 // Instructions: Create an array of at least five numbers in random order. Write the code to sort this array in ascending order.
+
+const numbers = [4, 19, 28, 37, 12, 69, 3]
+function sortAsc(a, b){
+  if(a>b) return 1
+  if(a<b) return -1
+  return 0
+}
+console.log(numbers.sort(sortAsc))
