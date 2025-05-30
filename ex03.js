9b9fec897c41a9c35996581e2bf8538647c40e59
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-29 20:54:21 -0700

    ex03 completed

--- ex03.js
+++ ex03.js
@@ -1,3 +1,7 @@
 // Reverse Exercise
 // Instructions: Create an array of at least five numbers. Write code to reverse the order of the elements in the array.
 
+const numbers = [88, 54, 60, 39, 21, 18, 93, 4, 9]
+const numReverse = numbers.reverse()
+console.log(numReverse)
+
